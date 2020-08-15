c63118ae679bbc9b8cc96b7ac15673fdc1d4a489
eduardo3g <eduardosbrasil10@gmail.com>
2020-08-15 14:09:03 -0300

    feat: Storing users in the database.

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -28,4 +28,4 @@ class UserController {
   }
 }
 
-export default UserController;
+export default new UserController();
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import routes from './routes';
+import 'reflect-metadata';
+
+import './database';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log('The server is running on port 3333 🔥'));
