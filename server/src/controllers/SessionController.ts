db9efb23a47e2d8c1011ebfffb8cab64bc99f560
eduardo3g <eduardosbrasil10@gmail.com>
2020-08-15 14:36:46 -0300

    feat: Creating user sessions.

---
+++ server/src/controllers/SessionController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import UserRepository from '../repositories/UserRepository';
+
+class SessionController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { username, password } = request.body;
+
+    const userRepository = getCustomRepository(UserRepository);
+
+    const user = await userRepository.findOne({ username });
+
+    if (!user) {
+      return response
+        .status(400)
+        .json({ error: `THe user "${username}" was not found.` });
+    }
+
+    const passwordMatches = await compare(password, user.password);
+
+    if (!passwordMatches) {
+      return response
+        .status(400)
+        .json({ error: 'Incorrect password or username.' });
+    }
+
+    const token = sign({}, 'e5036c188394f4f81a661fb325f21860', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return response.json({
+      token,
+      user,
+    });
+  }
+}
+
+export default new SessionController();
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+
 import UserController from './controllers/UserController';
+import SessionController from './controllers/SessionController';
 
 const router = Router();
 
 router.post('/users', UserController.create);
+
+router.post('/sessions', SessionController.create);
 
 export default router;
